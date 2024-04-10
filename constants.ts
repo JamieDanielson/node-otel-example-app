2d14a96c8bb0e229d4b5132692bc12aed8613d55
JamieDanielson <jamieedanielson@gmail.com>
2024-04-10 18:55:39 -0400

    use node auto-instr and constants

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const SERVICE_NAME = 'user-service';
+export const SERVICE_VERSION = '1.0';
+export const ENVIRONMENT = 'development';
+export const USERNAME_ATTRIBUTE = 'app.user.name';
+export const USERID_ATTRIBUTE = 'app.user.id';
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { Resource } from '@opentelemetry/resources';
@@ -9,39 +11,46 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 import { configureCompositeExporter } from './composite-exporter';
 import { ConsoleTraceLinkExporter } from './spanlinkexporter';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import {
-  ExpressInstrumentation,
-  ExpressLayerType,
-} from '@opentelemetry/instrumentation-express';
-
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { ExpressLayerType } from '@opentelemetry/instrumentation-express';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
-// set to debug to see in console
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+import { ENVIRONMENT } from './constants';
 
-const sdk = new NodeSDK({
-  resource: new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: 'js-backend',
-    [SEMRESATTRS_SERVICE_VERSION]: '1.0',
-    'my-attribute': 'my-value',
-  }),
-  traceExporter: configureCompositeExporter([
-    new OTLPTraceExporter(),
-    new ConsoleTraceLinkExporter(),
-  ]),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter(),
-  }),
-  instrumentations: [
-    new HttpInstrumentation(),
-    new ExpressInstrumentation({
-      // ignoreLayersType: [
-        // ExpressLayerType.MIDDLEWARE,
-        // ExpressLayerType.REQUEST_HANDLER,
-        // ExpressLayerType.ROUTER,
-      // ],
+export const instrument = (serviceName: string) => {
+  // set to debug to see in console
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+  const sdk = new NodeSDK({
+    resource: new Resource({
+      [SEMRESATTRS_SERVICE_NAME]: serviceName,
+      [SEMRESATTRS_SERVICE_VERSION]: '1.0',
+      environment: ENVIRONMENT,
     }),
-  ],
-});
+    traceExporter: configureCompositeExporter([
+      // set headers in env vars
+      // OTEL_EXPORTER_OTLP_HEADERS: 'x-honeycomb-team=api-key'
+      new OTLPTraceExporter(),
+      new ConsoleTraceLinkExporter(),
+    ]),
+    metricReader: new PeriodicExportingMetricReader({
+      // set headers in env vars
+      exporter: new OTLPMetricExporter(),
+    }),
+    instrumentations: [
+      getNodeAutoInstrumentations({
+        // only instrument fs if it is part of another trace
+        '@opentelemetry/instrumentation-fs': {
+          requireParentSpan: true,
+        },
+        '@opentelemetry/instrumentation-express': {
+          ignoreLayersType: [
+            ExpressLayerType.MIDDLEWARE,
+            ExpressLayerType.REQUEST_HANDLER,
+          ],
+        },
+      }),
+    ],
+  });
 
-sdk.start();
+  sdk.start();
+};
